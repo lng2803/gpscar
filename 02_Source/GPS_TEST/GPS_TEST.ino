0f96f11a862b9c0346378f9cd71c3a819cbe0e52
lng2803 <ppl.280302@gmail.com>
2023-09-23 21:08:47 +0700

    1

--- 02_Source/GPS_TEST/GPS_TEST.ino
+++ 02_Source/GPS_TEST/GPS_TEST.ino
@@ -1,30 +1,24 @@
-#include <HardwareSerial.h>
-#include <TinyGPS++.h>
+#include <HardwareSerial.h> //Thư viện uart của esp32
+#include <TinyGPS++.h> //Thư viện module GPS NEO 6M v2
 
-HardwareSerial ss(2);
+HardwareSerial ss(2); //Chọn cổng UART 2
 
-TinyGPSPlus gps;
+TinyGPSPlus gps; //Khởi tạo biến cho module GPS
 
 void setup() {
-  // put your setup code here, to run once:
-  ss.begin(9600, SERIAL_8N1, 16, 17);
-  Serial.begin(115200);
-
+  ss.begin(9600, SERIAL_8N1, 16, 17); //Chọn baudrate là 9600 (vì module dùng 9600)
+  //chân 16 17 RX TX cho ngõ ra
+  Serial.begin(115200); // Chọn baudrate để giao tiếp với máy tính
 }
 
 void loop() {
-  // while (ss.available() > 0){
-  //   // get the byte data from the GPS
-  //   byte gpsData = ss.read();
-  //   Serial.write(gpsData);
-  // }
-    while (ss.available() > 0){
-    gps.encode(ss.read());
-    if (gps.location.isUpdated()){
-      Serial.print("Latitude= "); 
-      Serial.print(gps.location.lat(), 6);
-      Serial.print(" Longitude= "); 
-      Serial.println(gps.location.lng(), 6);
+  while (ss.available() > 0){ //Trong khi UART2 có tín hiệu
+    gps.encode(ss.read()); //Đọc giá trị
+    if (gps.location.isUpdated()){ //Cập nhật giá trị GPS
+      Serial.print("Latitude= "); //Vĩ độ
+      Serial.print(gps.location.lat(), 6); //In giá trị vĩ độ ra
+      Serial.print(" Longitude= "); //Kinh độ
+      Serial.println(gps.location.lng(), 6); //In giá trị kinh độ ra
     }
   }
 }
